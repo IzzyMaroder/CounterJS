c95b73831cebd5c2035d95edefc379bc06d867c0
IzzyMaroder <86668744+IzzyMaroder@users.noreply.github.com>
2022-04-04 18:43:48 -0400

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,56 @@
-let counterDisplayElem = document.querySelector('.counter-display');
-let minoreElem = document.querySelector('.minore');
-let maggioreElem = document.querySelector('.maggiore');
-let resetElem = document.querySelector('.reset');
-let count = 0;
-
-updateDisplay();
-
-maggioreElem.addEventListener("click", () => {
-    count++;
-    updateDisplay();
-});
 
 
-minoreElem.addEventListener("click", () => {
-    if (count > 0) count--;
-    updateDisplay();
-});
+let containerDiv = document.createElement('div');
+let containerId = document.createAttribute('id');
+containerId.value = 'container';
+containerDiv.setAttributeNode(containerId);
+document.body.appendChild(containerDiv);
 
-resetElem.addEventListener("click", () => {
-    count = 0;
-    updateDisplay();
-})
-function updateDisplay() {
-    counterDisplayElem.innerHTML = count;
-};
+let plusBtn = document.createElement('button');
+let plusId = document.createAttribute('id');
+plusId.value = 'plus';
+plusBtn.setAttributeNode(plusId);
+containerDiv.appendChild(plusBtn);
+plusBtn.innerHTML = '+';
+
+
+
+let counterValueDiv = document.createElement('div');
+let counterValueId = document.createAttribute('id');
+counterValueId.value = 'counter-value';
+counterValueDiv.setAttributeNode(counterValueId);
+containerDiv.appendChild(counterValueDiv);
+counterValueDiv.innerHTML = 0;
+
+let minusBtn = document.createElement('button');
+let minusId = document.createAttribute('id');
+minusId.value = 'minus';
+minusBtn.setAttributeNode(minusId);
+containerDiv.appendChild(minusBtn);
+minusBtn.innerHTML = '-';
+
+
+let resetBtn = document.createElement('button');
+let resetId = document.createAttribute('id');
+resetId.value = 'reset';
+resetBtn.setAttributeNode(resetId);
+document.body.appendChild(resetBtn);
+resetBtn.innerHTML = 'Reset';
+
+
+let increase = function() {
+    counterValueDiv.innerHTML++;
+}
+plusBtn.addEventListener('click', increase);
+
+let reduce = function() {
+   if(counterValueDiv.innerHTML > 0){
+    counterValueDiv.innerHTML--;
+   }
+}
+minusBtn.addEventListener('click', reduce);
+
+let restart = function() {
+    counterValueDiv.innerHTML = 0;
+}
+resetBtn.addEventListener('click', restart);
